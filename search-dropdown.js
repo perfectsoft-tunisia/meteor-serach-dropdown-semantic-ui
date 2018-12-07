2601763b832186982428fcfd62fac27a455acb5e
Leila <chamtouri.leila93@gmail.com>
2018-12-07 18:06:05 +0100

    load dropdown options without typing

--- search-dropdown.js
+++ search-dropdown.js
@@ -5,7 +5,27 @@ import {Mongo} from 'meteor/mongo';
 
 Template.SearchDropdown.onCreated(function () {
     this.ResultsCollection = new Mongo.Collection(null);
-    this.textSearchCollection = new Mongo.Collection(null);    
+    this.textSearchCollection = new Mongo.Collection(null); 
+
+    const dropdownOptions = this.data.dropdown;
+    if (this.data.smallSet) {
+        const querry = '.*';
+        const methodName = 'SearchDropdown_' + dropdownOptions.id;
+        const labelKey = dropdownOptions.labelKey;
+        const valueKey = dropdownOptions.valueKey;
+
+        Meteor.call(methodName, querry, (error, result) => {
+            if (!error) {     
+                result.forEach((data) => {           
+                    this.ResultsCollection.upsert(data[valueKey], {$set: {
+                        name: data[labelKey],
+                        value: data[valueKey]
+                    }});
+                });
+            }
+        });
+    }
+
 });
 
 Template.SearchDropdown.helpers({
@@ -27,16 +47,20 @@ Template.SearchDropdown.onRendered(function () {
 
     const $dropdown = this.$('.dropdown');
     let template = this;
+
+
     if (this.data.onChange) {
         const onChange = function (val, text, $elem) {
             template.currentOption = {label: text, value: val};
             template.data.onChange(val, text, $elem, template.data.id);
         }
         dropdownOptions.onChange = onChange;
+        
     } else {
         dropdownOptions.onChange = function (val, text) {
             template.currentOption = {label: text, value: val};
         };
+
     }
 
     if (this.data.placeholder) {
@@ -48,7 +72,7 @@ Template.SearchDropdown.onRendered(function () {
             name: this.data.currentOption.label,
             value: this.data.currentOption.value,
             _id: this.data.currentOption.value,
-        })
+        });
     } else {
         this.currentOption = {};
     }
@@ -58,14 +82,20 @@ Template.SearchDropdown.onRendered(function () {
     const inputEvent = $dropdown.dropdown('get inputEvent');
     const $search = $dropdown.find('.search');
 
-    this.autorun(() => { 
-        $dropdown.dropdown('change values', this.ResultsCollection.find({}).fetch());
+    this.autorun(() => {
+        let dropdownOptions = this.ResultsCollection.find({}).fetch();
+        const oldValue = $dropdown.dropdown('get value');
+        $dropdown.dropdown('change values', dropdownOptions);
+        if (this.data.smallSet) {
+            $dropdown.dropdown('set selected', oldValue);
+        }
         $search.trigger(inputEvent)
     });
 
     if (this.data.currentOption) {
         this.currentOption = this.data.currentOption;
         $dropdown.dropdown('set selected', this.currentOption.value);
+
     } else {
         this.currentOption = {};
     }
@@ -73,6 +103,10 @@ Template.SearchDropdown.onRendered(function () {
 
 Template.SearchDropdown.events({
     'keyup input.search': _.debounce(function (event, template) {
+        if (template.data.smallSet) {
+            return 
+        }
+
         event.preventDefault();
         const query = $(event.currentTarget).val();
         const dropdownOptions = template.data.dropdown;
@@ -98,7 +132,7 @@ Template.SearchDropdown.events({
                         }});
                     });
                 }
-            });
+            });        
         }        
     },300),
 });
